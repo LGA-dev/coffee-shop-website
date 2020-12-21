fa27e8cff184ae4a3eab6a805a17a26fb5377682
Luciano Alcaraz <LGA-dev@hotmail.com>
2020-12-21 00:32:21 -0300

    Code cleanup

--- js/main.js
+++ js/main.js
@@ -1,138 +1,114 @@
+/* 
+======================================================
+00. Global variables and constants
+01. Buttons event listener
+02. Card type function
+03. Navbar color function
+04. Navbar background-color toggle fix
+05. Delivery status function
+====================================================== 
+*/
+
+// **** 00. Global variables and constants ****
+
+// Popover initialization
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+
+// Datepicker initialization
+$('#datepicker').datepicker({
+  uiLibrary: 'bootstrap4'
+});
+
+// AOS initialization
+AOS.init({
+  delay: 200,
+});
+
+// Buttons constants
+const allTypesButton = document.getElementById('all-types-button');
+const drinksButton = document.getElementById('drinks-button');
+const snacksButton = document.getElementById('snacks-button');
+const desertsButton = document.getElementById('desserts-button');
+
+// Cards constants
+const allMenuCards = document.querySelectorAll('.drinks, .snacks, .desserts');
+const drinkCards = document.querySelectorAll('.drinks');
+const snackCards = document.querySelectorAll('.snacks');
+const dessertCards = document.querySelectorAll('.desserts');
+// let currentActiveCardType = "";
+
+// Navbar constants
+const navigationBar = document.getElementById("navbar");
+const websiteLogo = document.getElementById("website-logo");
+
+// Radio and Input constants
+const pickUpRadio =  document.getElementById('custom-radio-pickup');
+const deliveryAddressInput =  document.getElementById('delivery-address-input');
+const cashAtLocationRadio =  document.getElementById('custom-radio-cash-at-location');
+const creditCardNumberInput =  document.getElementById('cc-number');
+const creditCardNameInput =  document.getElementById('cc-name');
+const creditCardDateInput =  document.getElementById('cc-date');
+const creditCardCVCInput =  document.getElementById('cc-cvc');
+
+// Toggler button constant
+const togglerButton = document.getElementById("toggler");
 
 
 
-// ## Menu buttons section ##
+// **** 01. Buttons event listener ****
+
+// On click, this will display only the selected card type
+allTypesButton.addEventListener("click", displayCardType);
+drinksButton.addEventListener("click", displayCardType);
+snacksButton.addEventListener("click", displayCardType);
+desertsButton.addEventListener("click", displayCardType);
 
 
 
-// Menu buttons variables
-let buttonAll = document.getElementById('button-all');
-let buttonDrinks = document.getElementById('button-drinks');
-let buttonDeserts = document.getElementById('button-deserts');
-let buttonSnacks = document.getElementById('button-snacks');
+// **** 02. Card type function ****
 
-// This function will show all cards
-let selectTypeAll = function () {
-  // Select all card types
-  let cardsToShow = document.querySelectorAll('.deserts,.snacks,.drinks');
-
-  // Apply a fadein effect to all card types
-  for (let i = 0; i < cardsToShow.length; i++) {
-    cardsToShow[i].classList.remove('fadeOut');
-    cardsToShow[i].classList.add('fadeIn');
+// This function will display the active card type based on the clicked button type
+function displayCardType() {
+  // Set the active card type
+  let currentActiveCardType = this.id;
+  // console.log("The current active type is: " + currentActiveCardType);
+  for(let i = 0; i < allMenuCards.length; i++) {
+    // Hide all the cards
+    allMenuCards[i].style.display = "none";
+    // Display the menu cards the match the current active type
+    if (currentActiveCardType === "all-types-button") {
+      console.log("Display all cards");
+      allMenuCards[i].style.display = "block";
+      allMenuCards[i].classList.add('fadeIn');
+    } else if (currentActiveCardType === "drinks-button" && allMenuCards[i].classList.contains('drinks')) {
+      console.log("Display all drinks cards");
+      allMenuCards[i].style.display = "block";
+      allMenuCards[i].classList.add('fadeIn');
+    } else if (currentActiveCardType === "snacks-button" && allMenuCards[i].classList.contains('snacks')) {
+      console.log("Display all snacks cards");
+      allMenuCards[i].style.display = "block";
+      allMenuCards[i].classList.add('fadeIn');
+    } else if (currentActiveCardType === "desserts-button" && allMenuCards[i].classList.contains('desserts')) {
+      console.log("Display all desserts cards");
+      allMenuCards[i].style.display = "block";
+      allMenuCards[i].classList.add('fadeIn');
+    } else {
+      console.log("Test");
+    }
   }
-
-  // This will highlight the clicked button in the menu
-  buttonAll.classList.remove('disabled');
-  buttonDrinks.classList.add('disabled');
-  buttonDeserts.classList.add('disabled');
-  buttonSnacks.classList.add('disabled');
-
-  // Console log for test
-  // console.log(cardsToHide.length);
-  // console.log(cardsToShow.length);
-}
-
-// This function will show all drinks cards
-let selectDrinksType = function () {
-  // Select card types to hide
-  let cardsToHide = document.querySelectorAll('.deserts,.snacks');
-  // Select card types to show
-  let cardsToShow = document.querySelectorAll('.drinks');
-
-  // Apply a fadeout effect to the previously selected card types
-  for (let i = 0; i < cardsToHide.length; i++) {
-    cardsToHide[i].classList.add('fadeOut');
-  }
-
-  // Apply a fadein effect to the previously selected card types
-  for (let i = 0; i < cardsToShow.length; i++) {
-    cardsToShow[i].classList.remove('fadeOut');
-    cardsToShow[i].classList.add('fadeIn');
-  }
-
-  // This will highlight the clicked button in the menu
-  buttonAll.classList.add('disabled');
-  buttonDrinks.classList.remove('disabled');
-  buttonDeserts.classList.add('disabled');
-  buttonSnacks.classList.add('disabled');
-
-  // Console log for tests
-  // console.log(cardsToHide.length)
-  // console.log(cardsToShow.length)
-}
-
-// Show deserts cards
-let selectDesertsType = function () {
-  // Select card types to show and hide
-  let cardsToHide = document.querySelectorAll('.drinks,.snacks');
-  let cardsToShow = document.querySelectorAll('.deserts');
-
-  // Apply a fadeout effect to the previously selected card types
-  for (let i = 0; i < cardsToHide.length; i++) {
-    cardsToHide[i].classList.add('fadeOut');
-  }
-
-  // Apply a fadein effect to the previously selected card types
-  for (let i = 0; i < cardsToShow.length; i++) {
-    cardsToShow[i].classList.add('fadeIn');
-    cardsToShow[i].classList.remove('fadeOut');
-  }
-
-  // This will highlight the clicked button in the menu
-  buttonAll.classList.add('disabled');
-  buttonDrinks.classList.add('disabled');
-  buttonDeserts.classList.remove('disabled');
-  buttonSnacks.classList.add('disabled');
-
-  // Console log for tests
-  // console.log(cardsToHide.length)
-  // console.log(cardsToShow.length)
-}
-
-// Show snacks cards
-let selectSnacksType = function () {
-  // Select card types to show and hide
-  let cardsToHide = document.querySelectorAll('.deserts,.drinks');
-  let cardsToShow = document.querySelectorAll('.snacks');
-
-  // Apply a fadeout effect to the previously selected card types
-  for (let i = 0; i < cardsToHide.length; i++) {
-    cardsToHide[i].classList.add('fadeOut');
-  }
-
-  // Apply a fadein effect to the previously selected card types
-  for (let i = 0; i < cardsToShow.length; i++) {
-    cardsToShow[i].classList.remove('fadeOut');
-    cardsToShow[i].classList.add('fadeIn');
-  }
-
-  // This will highlight the clicked button in the menu
-  buttonAll.classList.add('disabled');
-  buttonDrinks.classList.add('disabled');
-  buttonDeserts.classList.add('disabled');
-  buttonSnacks.classList.remove('disabled');
-
-  // Console log for tests
-  // console.log(cardsToHide.length)
-  // console.log(cardsToShow.length)
 }
 
 
 
-// Change navbar background color on scroll
+// **** 03. Navbar color function ****
 
-
-
-// Navbar variables
-let navigationBar = document.getElementById("navbar");
-let websiteLogo = document.getElementById("website-logo");
-
+// This function will change the navbar background color and the image on scroll
 function changeNavbarBackground() {
   window.addEventListener("scroll", () => {
       // Window top position variable
       let fromTop = window.scrollY;
-
       // Change bg color when scrolled
       if (fromTop > 1) {
         navigationBar.classList.add("bg-white");
@@ -148,7 +124,6 @@ function changeNavbarBackground() {
         navigationBar.classList.add("navbar-dark");
         navigationBar.classList.remove("shadow");
       }
-
       // Change navbar image
       if (fromTop > 0) {
           websiteLogo.src = 'img/brew-right-logo-dark.png';
@@ -161,16 +136,29 @@ changeNavbarBackground();
 
 
 
-// Delivery status check
-// This will disable certain inputs if the radio is checked
+// **** 04. Navbar background-color toggle fix ****
+// This is the workaround I took for the problem that I had with the navbar on mobile.
+
+// If the navbar toggle button gets clicked and the navbar isn't scrolled, 
+// the navbar container background color would stay transparent, making it hard to read the navbar items.
+
+// This function fix that by applying a white background to the navbar and dark text to the navbar items.
+// https://imgur.com/a/sy2dfFd
+
+// Execute function on button click
+togglerButton.addEventListener('click', function() {
+  navigationBar.classList.add("bg-white");
+  navigationBar.classList.remove("bg-transparent");
+  navigationBar.classList.add("navbar-light");
+  navigationBar.classList.remove("navbar-dark");
+  websiteLogo.src = 'img/brew-right-logo-dark.png';
+});
 
 
 
-// Radio and Input variables
-let pickUpRadio =  document.getElementById('custom-radio-pickup');
-let deliveryAddressInput =  document.getElementById('delivery-address-input');
+// **** 05. Delivery status function ****
 
-// This will check if the pickup radio is checked
+// This function will check if the pickup radio is checked
 function deliveryRadioCheck() {
   // If true, add the disabled attribute to the input, otherwise remove it
   if (pickUpRadio.checked) {
@@ -178,25 +166,9 @@ function deliveryRadioCheck() {
   } else {
     deliveryAddressInput.removeAttribute('disabled');
   }
-
-  // window.alert("test");
 }
 
-
-
-// Payment radio check
-// This will disable certain inputs if the radio is checked
-
-
-
-// Radio and Input variables
-let cashAtLocationRadio =  document.getElementById('custom-radio-cash-at-location');
-let creditCardNumberInput =  document.getElementById('cc-number');
-let creditCardNameInput =  document.getElementById('cc-name');
-let creditCardDateInput =  document.getElementById('cc-date');
-let creditCardCVCInput =  document.getElementById('cc-cvc');
-
-// This will check if the cash-at-location radio is checked
+// This function will check if the cash-at-location radio is checked
 function paymentRadioCheck() {
   // If true, add the disabled attribute to the inputs, otherwise remove it
   if (cashAtLocationRadio.checked) {
@@ -211,49 +183,3 @@ function paymentRadioCheck() {
     creditCardCVCInput.removeAttribute('disabled');
   }
 }
-
-
-
-// Change navbar color on toggle
-
-// This is the workaround I took for the problem that I had with the navbar on mobile.
-
-// If the navbar toggle button gets clicked and the navbar isn't scrolled, 
-// the navbar container background color would stay transparent, making it hard to read the navbar items.
-
-// This function fix that by applying a white background to the navbar and dark text to the navbar items.
-// https://imgur.com/a/sy2dfFd
-
-
-// Toggle button variable
-let togglerButton = document.getElementById("toggler");
-
-// Execute function on button click
-togglerButton.addEventListener('click', function() {
-  navigationBar.classList.add("bg-white");
-  navigationBar.classList.remove("bg-transparent");
-  navigationBar.classList.add("navbar-light");
-  navigationBar.classList.remove("navbar-dark");
-  websiteLogo.src = 'img/brew-right-logo-dark.png';
-});
-
-
-
-// ## Init section ##
-
-
-
-// Popover
-$(function () {
-  $('[data-toggle="popover"]').popover()
-})
-
-// Datepicker
-$('#datepicker').datepicker({
-  uiLibrary: 'bootstrap4'
-});
-
-// AOS
-AOS.init({
-  delay: 200,
-});
