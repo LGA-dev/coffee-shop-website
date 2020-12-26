9c51b24346c5d3ec8403b9ca01e081f66b60b23d
Luciano Alcaraz <LGA-dev@hotmail.com>
2020-12-26 08:32:55 -0300

    Update files

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,82 @@
-"use strict";
-
-// Load plugins
-const browsersync = require("browser-sync").create();
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
 const del = require("del");
-const gulp = require("gulp");
 const merge = require("merge-stream");
+const autoprefixer = require('gulp-autoprefixer');
 
-// BrowserSync
-function browserSync(done) {
-  browsersync.init({
-    server: {
-      baseDir: "./"
-    },
-    port: 3000
-  });
-  done();
-}
 
-// BrowserSync reload
-function browserSyncReload(done) {
-  browsersync.reload();
-  done();
-}
 
 // Clean vendor
 function clean() {
   return del(["./vendor/"]);
 }
 
+
 // Bring third party dependencies from node_modules into vendor directory
 function modules() {
-  // Bootstrap
-  var bootstrap = gulp.src('./node_modules/bootstrap/dist/**/*')
-    .pipe(gulp.dest('./vendor/bootstrap'));
-  // jQuery
-  var jquery = gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
-    ])
+  // jQuery files
+  let jquery = gulp.src('./node_modules/jquery/dist/*')
     .pipe(gulp.dest('./vendor/jquery'));
-  return merge(bootstrap, jquery);
+  
+  // Popper.js JS file
+  let popper = gulp.src('./node_modules/@popperjs/core/dist/umd/popper.min.js')
+    .pipe(gulp.dest('./vendor/popper'));
+  
+  // Bootstrap JS files
+  let bootstrap_js = gulp.src([
+      './node_modules/bootstrap/dist/js/bootstrap.min.js',
+      './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+      ]
+    )
+    .pipe(gulp.dest('./vendor/bootstrap/js'));
+  
+  // Bootstrap SCSS files
+  let bootstrap_scss = gulp.src('./node_modules/bootstrap/scss/**/*')
+    .pipe(gulp.dest('./vendor/bootstrap/scss'));
+  
+  // Font Awesome CSS files
+  let fontawesome_js = gulp.src('./node_modules/@fortawesome/fontawesome-free/css/all.min.css')
+    .pipe(gulp.dest('./vendor/fontawesome/css'));
+  
+  // Font Awesome CSS files
+  let fontawesome_fonts = gulp.src('./node_modules/@fortawesome/fontawesome-free/webfonts/*')
+    .pipe(gulp.dest('./vendor/fontawesome/webfonts'));
+
+  return merge(jquery, popper, bootstrap_js, bootstrap_scss, fontawesome_js, fontawesome_fonts);
 }
 
-// Watch files
-function watchFiles() {
-  gulp.watch("./**/*.css", browserSyncReload);
-  gulp.watch("./**/*.html", browserSyncReload);
+
+// Compile scss files into a single minified css file
+// This will also compile bootstrap
+function style() {
+  return gulp.src('src/scss/**/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream());
 }
 
-// Define complex tasks
-const vendor = gulp.series(clean, modules);
-const build = gulp.series(vendor);
-const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
+
+// Create a local server and watch for changes
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: ".",
+      index: "./index.html"
+    }
+  });
+  gulp.watch('./**/*.scss', style)
+  gulp.watch('./**/*.html').on('change',browserSync.reload)
+  gulp.watch('./**/*.js').on('change', browserSync.reload)
+}
+
+
+// Define tasks
+const build = gulp.series(clean, modules, style);
+
 
 // Export tasks
-exports.clean = clean;
-exports.vendor = vendor;
+exports.default = build;
 exports.build = build;
 exports.watch = watch;
-exports.default = build;
