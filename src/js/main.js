7b3d7207d2c8cf0733dc7ded9854e251c84a80d5
Luciano Alcaraz <LGA-dev@hotmail.com>
2021-01-09 16:01:26 -0300

    Update site

--- src/js/main.js
+++ src/js/main.js
@@ -1,63 +1,3 @@
-// Enable popovers (popovers aren't going to work without this)
-// $(function () {
-//   $('[data-toggle="popover"]').popover()
-// })
-
-
-
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
-})
-
-
-// Check if jquery is working
-function checkJquery() {
-  const btn = document.getElementById("check-jquery-button");
-  if (window.jQuery) {
-    btn.innerHTML = 'Jquery is working <i class="fas fa-smile"></i>';
-  } else {
-    alert("jQuery isn't working");
-  }
-}
-
-// Check if fontawesome is working
-function checkFontAwesome() {
-  const btn = document.getElementById("check-font-awesome-button");
-  const fa = document.createElement("svg");
-
-  fa.className = "fa fa-ambulance";
-  fa.style.position = "absolute";
-  fa.style.left = 0;
-  fa.style.top = 0;
-  fa.style.visibility = "hidden";
-  document.body.appendChild(fa);
-
-  window.setTimeout(function() {
-    let widthBefore = fa.offsetWidth;
-    fa.style.fontFamily = "asdf";
-    let widthAfter = fa.offsetWidth;
-    if (widthBefore === widthAfter) {
-      alert("Font Awesome isn't working");
-    } else {
-      btn.innerHTML = 'Font Awesome is working <i class="fas fa-smile"></i>';
-    }
-    // document.body.removeChild(fa);
-  }, 50);
-}
-
-// Check if jquery is working
-function checkBootstrap() {
-  const btn = document.getElementById("check-bootstrap-button");
-  if (typeof($.fn.modal) !== 'undefined') {
-    btn.innerHTML = 'Bootstrap is working <i class="fas fa-smile"></i>';
-  } else {
-    alert("Bootstrap isn't working");
-  }
-}
-
-
-// Old code
 /* 
 ======================================================
 00. Global variables and constants
@@ -69,37 +9,7 @@ function checkBootstrap() {
 ====================================================== 
 */
 
-// **** 00. Global variables and constants ****
-
-const pickUpRadio = document.getElementById('pickup-radio-button');
-const deliveryAddressInput = document.getElementById('delivery-address-input');
-
-const cashAtLocationRadio =  document.getElementById('cash-at-location-payment-button');
-const creditCardNumberInput =  document.getElementById('cc-number');
-const creditCardNameInput =  document.getElementById('cc-name');
-const creditCardDateInput =  document.getElementById('cc-date');
-const creditCardCVCInput =  document.getElementById('cc-cvc');
-
-// Popover initialization
-$(function () {
-  $('[data-toggle="popover"]').popover()
-})
-
-
-
-// Datepicker initialization
-$('#datepicker').datepicker({
-  uiLibrary: 'bootstrap4'
-});
-
-
-
-// AOS initialization
-AOS.init({
-  delay: 200,
-});
-
-
+// **** 00. Global constants and initializations ****
 
 // Buttons constants
 const allTypesButton = document.getElementById('all-types-button');
@@ -112,18 +22,38 @@ const allMenuCards = document.querySelectorAll('.drinks, .snacks, .desserts');
 const drinkCards = document.querySelectorAll('.drinks');
 const snackCards = document.querySelectorAll('.snacks');
 const dessertCards = document.querySelectorAll('.desserts');
-// let currentActiveCardType = "";
 
 // Navbar constants
 const navigationBar = document.getElementById("nav");
 const websiteLogo = document.getElementById("website-logo");
 const togglerButton = document.getElementById("toggler");
 
+// Radio constants
+const pickUpRadio = document.getElementById('pickup-radio-button');
+const cashAtLocationRadio =  document.getElementById('cash-at-location-payment-button');
 
-// Radio and Input constants
-// Address section
+// Input constants
+const deliveryAddressInput = document.getElementById('delivery-address-input');
+const creditCardNumberInput =  document.getElementById('cc-number');
+const creditCardNameInput =  document.getElementById('cc-name');
+const creditCardDateInput =  document.getElementById('cc-date');
+const creditCardCVCInput =  document.getElementById('cc-cvc');
 
-// Payment section
+// Popover initialization
+const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+const popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+// Datepicker initialization
+$('#datepicker').datepicker({
+  uiLibrary: 'bootstrap4'
+});
+
+// AOS initialization
+AOS.init({
+  delay: 200,
+});
 
 
 
